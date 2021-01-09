2d7deea6069c0b7157d2814fd0e7ebaef0bc3900
Karen Opoku <karenyaa@live.co.uk>
2021-01-09 20:31:07 +0000

    adding html, script & js files

---
+++ script.js
@@ -0,0 +1,44 @@
+// var startQuiz = document.getElementsByClassName("startBtn")
+//Pseudo code
+// When the 'Start Quiz' button is clicked, it should trigger a series of questions. 
+//Thus an event listener is required. You need to attach a click and a function
+//Parameters need to be set for answers. that is if answer is correct or wrong, this will be displayed on next screen
+//Timer needs to be created to countdown as soon as quiz is started
+//time is substracted from the clock when a question is answered wrong
+//when all questions are answered or timer reaches zero, the game ends (game over screen appears)
+//when game is over, it goes to another page for user to enter initials and save game
+
+//what to use? set interval, DOM manipulation, for loops, setInterval & clearInterval, functions and preventDefault
+document.getElementsByClassName("startBtn").addEventListener("click", function(){
+    alert("you've clicked the start button");
+});
+
+// Questions
+// 1. Commonly used data types do NOT include ___
+// -boolean
+// -numbers
+// -String
+// -prompts
+
+// 2.Which tag would you put your block of Javascript code?
+// - <javascript>
+// - <js>
+// - <script>
+// - <section>
+
+// 3. What does HTML stand for?
+// - HyperTech Markup Language
+// -HyperText Markup Language
+// - HypoText Mark Language
+
+// 4. Which property is used to change background colour?
+// -background-colour
+// -bgcolor
+// -color
+
+// 5.How do you call a function in Javascript?
+// - function myFunction ()
+// - function: myFunction()
+// - myFunction ()
+
+
